f7e25c07121a57051e75f313eba58b0ebea1ed13
m-usman02 <heyusman00@gmail.com>
2023-08-21 20:58:10 +0500

    sidebar extra code removed

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.layout.master');
+});
